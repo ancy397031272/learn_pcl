59b4665fe5509fef02fc0c9cb52bb6dc7b75b548
Ancheng Wu <ac.wu@rvbust.com>
2022-03-02 17:42:29 +0800

    .

--- 1.检查点是否有效/CheckIfPointIsValid.cpp
+++ 1.检查点是否有效/CheckIfPointIsValid.cpp
@@ -1,10 +1,24 @@
 
-#include<pcl/point_types.h>
-#include<pcl/common/point_tests.h>
+#include <iostream>
+#include <pcl/common/point_tests.h> // for pcl::isFinite
+#include <pcl/point_types.h>
 
+using namespace std;
 
-int main()
-{
-  pcl::Point
+int main() {
+  pcl::PointXYZ p_valid;
+  p_valid.x = 0;
+  p_valid.y = 0;
+  p_valid.z = 0;
+
+  cout << "IS p_valid valid? " << pcl::isFinite(p_valid) << endl;
+
+  // If any component is NaN, the point is not finite.
+  pcl::PointXYZ p_invalid;
+  p_invalid.x = std::numeric_limits<float>::quiet_NaN();
+  p_invalid.y = 0;
+  p_invalid.z = 0;
+  std::cout << "Is p_invalid valid? " << pcl::isFinite(p_invalid) << std::endl;
+
   return 0;
 }
