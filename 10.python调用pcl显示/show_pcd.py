58efcda925e58a244cfcaac513d32cf894cd2bc5
Ancheng Wu <ac.wu@rvbust.com>
2022-04-01 17:46:08 +0800

    .

---
+++ 10.python调用pcl显示/show_pcd.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import build.example as ex
+
+num = 1000
+pcd = [np.random.rand(num), np.random.rand(num), np.random.rand(num), np.random.rand(num), np.random.rand(num),
+       np.random.rand(num)]
+
+var = ex.showpcd(pcd)
